5730148970d0028a6457688834fd9b1a24b34346
yakirgoaron@gmail.com <yakirgoaron@gmail.com@687418a6-0840-4ee3-1b46-a6a781df1a0b>
2014-02-22 13:38:40 +0000

    dd

--- EX01/src/GameEngine/GameEngine.java
+++ EX01/src/GameEngine/GameEngine.java
@@ -6,10 +6,72 @@
 
 package GameEngine;
 
+import GameEngine.Exception.TooManyPlayersException;
+import java.util.ArrayList;
+
 /**
  *
  * @author yakir
  */
-public class GameEngine {
+public class GameEngine 
+{
+    ArrayList<Player> GamePlayers;
+    ArrayList<Card> GameDeck;
+    Dealer GameDealer;
+    int TopDeckCard;
+    int PlayerTurn;
+    final int NUMBER_PLAYERS = 6;
+    
+    
+    public GameEngine()
+    {
+        GamePlayers = new ArrayList<>();
+        GameDealer = new Dealer();
+        NewRound();
+    }
+    
+    private void NewRound()
+    {
+        GameDeck = Card.newDeck();
+        TopDeckCard = 0;
+        PlayerTurn = 0;
+    }
+    
+    
+    public void StartNewRound()
+    {
+        NewRound();
+    }
+    
+    public Card PullCard()
+    {
+        return GameDeck.get(TopDeckCard++);
+    }
+    
+    
+    public void AddPlayer(String Name) throws TooManyPlayersException
+    {
+        if(GamePlayers.size() == NUMBER_PLAYERS)
+        {
+            throw new TooManyPlayersException();
+        }
+        GamePlayers.add(new HumanPlayer());
+    }
+    
+    public void AddPlayer() throws TooManyPlayersException
+    {
+        if(GamePlayers.size() == NUMBER_PLAYERS)
+        {
+             throw new TooManyPlayersException();
+        }
+        GamePlayers.add(new CompPlayer());
+    }
+    
+    public Player GetCurrentPlayer()
+    {
+        return GamePlayers.get(PlayerTurn++);
+    }
+    
+    
     
 }
