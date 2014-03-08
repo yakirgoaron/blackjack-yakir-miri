4cb7c1b3253dcc73aede23e54b2108f86b131082
yakirgoaron@gmail.com <yakirgoaron@gmail.com@687418a6-0840-4ee3-1b46-a6a781df1a0b>
2014-03-08 14:57:33 +0000

    dd

---
+++ EX01/src/EngineLogic/Communicable.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package EngineLogic;
+
+/**
+ *
+ * @author yakir
+ */
+public interface Communicable 
+{
+    public static enum PlayerAction{
+        
+        DOUBLE("Double"), HIT("Hit"), SPLIT("Split"), STAY("Stay"), 
+        QUIT_GAME("Quit Game");
+        
+        private final String Description;
+        private static final int Size = PlayerAction.values().length;
+        
+        PlayerAction(String Description){
+            this.Description = Description;
+        }
+
+        public String getDescription() {
+            return Description;
+        }
+
+        public static int getSize() {
+            return Size;
+        }       
+    }
+    public static enum RoundAction{      
+        SAVE_GAME("Save and Exit game"), 
+        NEW_ROUND("New Round"); 
+        
+        private final String Description;
+        private static final int Size = PlayerAction.values().length;
+        
+        RoundAction(String Description){
+            this.Description = Description;
+        }
+
+        public String getDescription() {
+            return Description;
+        }
+
+        public static int getSize() {
+            return Size;
+        }       
+    }
+    
+    public PlayerAction GetWantedAction();
+    
+    public void PrintPlayerInfo();
+    
+    public RoundAction GetFinishGameAction();
+    
+    
+}
