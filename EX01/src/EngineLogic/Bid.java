1bb4c6025551c6aa008981c45eec1ca9a6542b5e
yakirgoaron@gmail.com <yakirgoaron@gmail.com@687418a6-0840-4ee3-1b46-a6a781df1a0b>
2014-03-08 16:31:09 +0000

    dd

--- EX01/src/EngineLogic/Bid.java
+++ EX01/src/EngineLogic/Bid.java
@@ -7,30 +7,28 @@
 package EngineLogic;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
  * @author yakir & miri
  */
-public class Bid 
+public class Bid extends Hand
 {
     private Double TotalBid;
-    private Hand BidCards;
   
     
     private void InIBid()
     {
         TotalBid = 0.0;
-        BidCards = new Hand();
+       
     }
     
     public Bid(Card FirstCard,Card SecondCard,Double BetValue)
     {
         InIBid();
         TotalBid = BetValue;
-        BidCards.AddCard(FirstCard);
-        BidCards.AddCard(SecondCard);
+        AddCard(FirstCard);
+        AddCard(SecondCard);
     }
     
     
@@ -40,7 +38,7 @@ public class Bid
         this.TotalBid = TotalBid;
         for (Card card : Cards) 
         {
-            BidCards.AddCard(card);  
+            AddCard(card);  
         }
     }
     
@@ -49,17 +47,12 @@ public class Bid
     {
         InIBid();
         TotalBid = BidWanted;
-        BidCards.AddCard(FirstCard);        
+        AddCard(FirstCard);        
     }
     
       
     public Double getTotalBid() {
         return TotalBid;
-    }
-
-    public int GetSumOfCards()
-    {
-        return this.BidCards.getSumCards();
     }
     
     public void DoubleBid(Card card)
@@ -70,16 +63,7 @@ public class Bid
     
     public void HitBid(Card card)
     {
-        BidCards.AddCard(card);
+        AddCard(card);
     }
-    
-    public int GetNumberOfCards()
-    {
-        return BidCards.GetNumberOfCards();
-    }
-    
-    public ArrayList<Card> getCards() {
-        return BidCards.getCards();
-    }
-    
+        
 }
