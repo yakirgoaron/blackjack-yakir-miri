dd0e9e9f09ac44f3992e87d4799e583dd78c4fb6
yakirgoaron@gmail.com <yakirgoaron@gmail.com@687418a6-0840-4ee3-1b46-a6a781df1a0b>
2014-03-22 08:42:02 +0000

    dd

--- BlackJackFX/src/blackjackfx/BlackJackFX.java
+++ BlackJackFX/src/blackjackfx/BlackJackFX.java
@@ -6,9 +6,15 @@
 
 package blackjackfx;
 
+import EngineLogic.GameEngine;
+import blackjackfx.Controllers.CreatePlayersScreenController;
+import java.io.IOException;
+import java.net.URL;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -21,25 +27,19 @@ import javafx.stage.Stage;
 public class BlackJackFX extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage) throws IOException
+    {
+       GameEngine BlackJackGame = new GameEngine();
+       FXMLLoader fxmlLoader = new FXMLLoader();
+       URL url = getClass().getResource("CreatePlayersScreen.fxml");
+       fxmlLoader.setLocation(url);
+       Parent root = (Parent)fxmlLoader.load(url.openStream());
+       CreatePlayersScreenController CreatePlayers = (CreatePlayersScreenController) fxmlLoader.getController();
+       
+       CreatePlayers.setBjGame(BlackJackGame);
+       Scene scene = new Scene(root);
+       primaryStage.setScene(scene);
+       primaryStage.show();
     }
 
     /**
