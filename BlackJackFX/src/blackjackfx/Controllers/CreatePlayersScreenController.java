65067c1ae2f0ec790259a415f7f698fe9a7b3f42
yakirgoaron@gmail.com <yakirgoaron@gmail.com@687418a6-0840-4ee3-1b46-a6a781df1a0b>
2014-03-22 08:42:13 +0000

    dd

--- BlackJackFX/src/blackjackfx/Controllers/CreatePlayersScreenController.java
+++ BlackJackFX/src/blackjackfx/Controllers/CreatePlayersScreenController.java
@@ -6,9 +6,18 @@
 
 package blackjackfx.Controllers;
 
+import EngineLogic.Exception.TooManyPlayersException;
+import EngineLogic.GameEngine;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -20,9 +29,39 @@ public class CreatePlayersScreenController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    private GameEngine BjGame;
+    @FXML
+    private Pane PlayerIn;
+    @FXML
+    private TextField TextName;
+    @FXML
+    private Button BtnAdd;
+    @FXML
+    private CheckBox IsHuman;
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+       //this.BtnAdd.onActionProperty().addListener(new );
     }    
     
+    @FXML
+    public void AddPlayer()
+    {
+        GameEngine eng = new GameEngine();
+        try {
+            eng.AddPlayer("Hello");
+            eng.AddPlayer();
+        }
+        catch (TooManyPlayersException ex) 
+        {
+            Logger.getLogger(CreatePlayersScreenController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    
+    public void setBjGame(GameEngine BjGame) {
+        this.BjGame = BjGame;
+    }
 }
